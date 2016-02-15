1eaf1885a8123590a156f4f719bae3f963c9e4d9
wuwweiwei <wuww5511@126.com>
2016-02-15 16:12:50 +0800

    实现类生成子类的功能

---
+++ extend.js
@@ -0,0 +1,20 @@
+/**
+ *NEJ扩展
+ */
+define([
+    'base/klass',
+    'base/util'
+], function (_k, _u, _p, _o, _f) {
+    //类的继承
+    Function.prototype._$getSub = function (_opts) {
+        
+        var _$$Sub = _k._$klass(),
+            _pro = _$$Sub._$extend(this);
+        
+        _u._$merge(_pro, _opts);
+        
+        if(!this.prototype.__init) this.prototype.__init = _f;
+        
+        return _$$Sub;
+    };
+})
