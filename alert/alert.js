280061d1239aeae7f7e28acbbf1dbe5b9666d6d6
wuwweiwei <wuww5511@126.com>
2016-04-22 18:23:45 +0800

    补充~

--- alert/alert.js
+++ alert/alert.js
@@ -15,7 +15,7 @@ define([
         this.__pos = new _pos._$$Widget(this);
         this.__inner = _opts.inner || this.__inner;
         if(typeof this.__inner === 'string'){
-            this.__body.innerHTML = _inner;
+            this.__body.innerHTML = this.__inner;
             this.__alert = this.__getFirstChild(this.__body);
         }
         else{
