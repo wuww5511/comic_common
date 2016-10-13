a7aeb1b0e06057e14724d714308aa47a62095896
wuwweiwei <wuww5511@126.com>
2016-10-13 19:53:50 +0800

    对NEJ的动画类进行封装；（原有动画类存在_$stop后动画仍执行的bug）

--- anim/animAbstract.js
+++ anim/animAbstract.js
@@ -1,12 +1,67 @@
+/**
+ *  需要考虑的case:
+ *  1.  在动画未结束时调用_$play
+ *  2.  动画停止后能否继续播放，回调函数能否正确执行（包括自动停止和手动停止） 
+ */
 define([
-    '../extend.js'
-], function (ext, p) {
+    '../extend.js',
+    'util/event'
+], function (ext, u, p) {
+       
+    var pro = {};
     
-   /**
-    * 动画类接口；同时需要实现onstop事件
-    */    
-   p._$$Anim = ext._$abstract([
-       "_$play",
-       "_$stop"
-   ]); 
+    pro.__reset = function (opts) {
+        this.__super(opts);
+        this.__cbEnd = null;
+        this.__isPlaying = false;
+    };
+    
+    pro._$play = function (cb) {
+        if(this.__isPlaying) return;
+        this.__isPlaying = true;
+        this.__cbEnd = cb;
+        this.__doPlay();
+    };
+    
+    pro._$stop = function () {
+        this.__doStop();
+        this.__onForcedEnd();
+    };
+    
+    pro._$isPlaying = function () {
+        return this.__isPlaying;
+    };
+    
+    pro.__doStop = function () {
+        
+    };
+    
+    pro.__doPlay = function () {
+        
+    };
+    
+    //强制停止后执行
+    pro.__onForcedEnd = function () {
+        this.__doEnd();
+    };
+    
+    //动画播放结束
+    pro.__onExecutedEnd = function () {
+        this.__doEnd();
+    };
+    
+    pro.__doEnd = function () {
+        this.__isPlaying = false;;
+        this.__cbEnd && this.__cbEnd();
+        this._$dispatchEvent('onstop');
+    };
+    
+    pro.__destroy = function () {
+        this._$stop();
+        this.__super(opts);
+        delete this.__cbEnd;
+        delete this.__isPlaying;
+    };
+    
+    p._$$Anim = u._$$EventTarget._$getSub(pro);
 });
---
+++ anim/animNej.js
@@ -0,0 +1,83 @@
+define([
+    './animAbstract.js',
+    'util/animation/easein',
+    'util/animation/easeout',
+    'util/animation/linear'
+], function (anim, easein, easeout, linear, p) {
+   
+    var pro = {};
+    
+    var klassMap = {
+        "easein": easein._$$AnimEaseIn,
+        "easeout": easeout._$$AnimEaseOut,
+        "linear": linear._$$AnimLinear
+    };
+    
+    /**
+     *  @param {Object} 
+     *      -   data {Array} 形如['easein', 1000, {}， {}]的数据
+     */
+    pro.__reset = function (opts) {
+        this.__super(opts);
+        this.__klass = klassMap[opts.data[0]];
+        this.__during = opts.data[1];
+        this.__from = opts.data[2];
+        this.__to = opts.data[3];
+        this.__anim = this.__getAnim();
+        
+    };
+    
+    pro.__doPlay = function () {
+        this.__anim._$play();
+    };
+    
+    pro.__doStop = function () {
+        this.__anim._$recycle();
+        this.__anim = this.__getAnim();
+        this.__doStep(this.__to);
+    };
+    
+    pro.__doStep = function (data) {
+        this._$dispatchEvent('onstep', data);
+    };
+    
+    pro.__onAnimStop = function () {
+        this.__onExecutedEnd();
+    };
+    
+    pro.__onStep = function (from, to, event) {
+        var percent = event.offset / 100;
+        var data = {};
+        
+        for(var i in from) {
+            data[i] = (to[i] - from[i]) * percent + from[i];
+        }
+        
+        this.__doStep(data);
+    };
+     
+    pro.__getAnim = function () {
+        return this.__klass._$allocate({
+            from: {
+                offset: 0
+            },
+            to: {
+                offset: 100
+            },
+            duration: this.__during,
+            onupdate: this.__onStep._$bind(this, this.__from, this.__to),
+            onstop: this.__onAnimStop._$bind(this)
+        });
+    };
+    
+    pro.__destroy = function () {
+        this.__super();
+        this.__anim._$recycle();
+        delete this.__klass;
+        delete this.__during;
+        delete this.__from;
+        delete this.__to;
+    };
+    
+    p._$$Anim = anim._$$Anim._$getSub(pro);
+});
